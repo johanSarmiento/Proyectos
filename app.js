8e5b1388673e859d8591560db442623bcbe3bf9b
johanSarmiento <johansarmiento47@gmail.com>
2023-02-06 23:29:22 -0600

    creaion de la interfas grafica

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-// for(let i = 0; i < 100; i++){
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-//     .then(response => response.json())
-//     .then(data => console.log(data.name))
-// }
+const body = document.getElementById("cartas")
+const cartasPokemon = document.createElement("div")
+const contImg = document.createElement("div")
+const contInfo = document.createElement("div")
+const imgPokemon = document.createElement("img")
+const nombrePokemon = document.createElement("h2")
+const tipoPokemon = document.createElement("p")
+const numPokemon = document.createElement("p")
+//ASIGNANDO LOS ATRIBUTOS
+cartasPokemon.setAttribute("class", "contenedor cartas")
+contImg.setAttribute("class", "contenedor contImg")
+contInfo.setAttribute("class", "contenedor contInfo")
+imgPokemon.setAttribute("class", "imgPokemon")
+nombrePokemon.setAttribute("class", "nombrePokemon")
+tipoPokemon.setAttribute("class", "tipoPokemon")
+numPokemon.setAttribute("class", "numPokemon")
+//ASIGNANDO HIJOS
+body.appendChild(cartasPokemon)
+cartasPokemon.appendChild(contImg)
+cartasPokemon.appendChild(contInfo) 
+contImg.appendChild(imgPokemon)
+contInfo.appendChild(nombrePokemon)
+contInfo.appendChild(tipoPokemon)
+contInfo.appendChild(numPokemon)
 
-let arreglo = ["johan","maria","miguel","kevin","jose"]
-// let body = document.getElementById("ejemplo")
-// const nombre = document.createElement("p")
-// body.appendChild(nombre)
-// nombre.textContent= "hola"
-for(let nombre of arreglo){
-    document.getElementById("ejemplo").innerHTML = nombre
+async function llamarApi(id) {
+    let api = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    let pokemos = await api.json()
+    console.log(pokemos);
+    llenarCarta(pokemos)
 }
+function llenarCarta(data) {
+    nombrePokemon.innerHTML = data.name
+    tipoPokemon.innerHTML = data.types[0].type.name
+    tipoPokemon.innerHTML = data.types[1].type.name
+    numPokemon.innerHTML = data.id
+}
+llamarApi(2)
