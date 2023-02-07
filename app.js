55bbea509a523255a1a5933f92d6402d3cf617b0
johanSarmiento <johansarmiento47@gmail.com>
2023-02-06 19:23:52 -0600

    hola

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-async function llamar () {
-
-    
-}
 // for(let i = 0; i < 100; i++){
 //     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
 //     .then(response => response.json())
 //     .then(data => console.log(data.name))
 // }
+
+let arreglo = ["johan","maria","miguel","kevin","jose"]
+// let body = document.getElementById("ejemplo")
+// const nombre = document.createElement("p")
+// body.appendChild(nombre)
+// nombre.textContent= "hola"
+for(let nombre of arreglo){
+    document.getElementById("ejemplo").innerHTML = nombre
+}
